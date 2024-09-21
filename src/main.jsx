1fd03e0a591bb34d85bfe026e285da715168fe14
Muhammad Muneeb <muneebdev18@gmail.com>
2024-09-22 04:03:40 +0500

    Update Redux & Toasts

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,24 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import "react-toastify/dist/ReactToastify.css";
+import { Slide, ToastContainer } from "react-toastify";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
     <App />
+    <ToastContainer
+        autoClose={2000}
+        hideProgressBar={true}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+        transition={Slide}
+      />
     </BrowserRouter>
   </StrictMode>,
-)
+);
